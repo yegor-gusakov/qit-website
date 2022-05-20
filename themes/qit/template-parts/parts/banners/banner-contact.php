918f1286a9c117bb20ff4a4d1ba4b1a7502fbbe3
Danil <daniil.lavronenko@qit.software>
2022-05-20 11:52:03 +0300

    Fixing bugs on contact form

--- themes/qit/template-parts/parts/banners/banner-contact.php
+++ themes/qit/template-parts/parts/banners/banner-contact.php
@@ -82,7 +82,57 @@ $contact_image_of_text = $args['contact_image_of_text'];
                     </div>
 					<?php
 					break;
+                default:
+					?>
+                    <div class="section__banner-contact-row-right col-lg-6 p-sm-5 d-none d-lg-block">
+                        <h5><?= $fact_title ?></h5>
+						<?php if ( $facts_repeater ): ?>
+                            <div class="row">
 
+								<?php foreach ( $facts_repeater as $fact ) : ?>
+									<?php
+									$fact_number = $fact['title'];
+									$fact_text   = $fact['text'];
+									?>
+                                    <div class="col-lg-6 align-items-stretch mb-5 ">
+                                    <div class="card  mb-3 border-0 border-bottom bg-transparent h-100"
+                                         style="max-width: 18rem;">
+                                        <div class="card-header p-0 border-0 bg-transparent">
+                                            <h5 class="card-title"><?= $fact_number ?>
+                                                %</h5></div>
+                                        <div class="card-body p-0 border-0">
+                                            <p class="card-text"><?= $fact_text ?></p>
+                                        </div>
+                                    </div>
+                                    </div><?php
+								endforeach; ?>
+                            </div>
+
+						<?php endif; ?>
+						<?php if ( $reviews_repeater ): ?>
+
+                            <div class="row">
+                                <ul class="list-group list-group-horizontal">
+									<?php foreach (
+										$reviews_repeater
+
+										as $review
+									) :
+										$review_image = $review['image']
+										?>
+
+                                        <li class="list-group-item border-0 bg-transparent">
+                                            <img
+                                                    src="<?= $review_image['url'] ?>"
+                                                    alt="<?= $review_image['alt'] ?>">
+                                        </li>
+									<?php endforeach; ?>
+                                </ul>
+                            </div>
+						<?php endif; ?>
+                    </div>
+					<?php
+					break;
 			endswitch; ?>
 
         </div>
